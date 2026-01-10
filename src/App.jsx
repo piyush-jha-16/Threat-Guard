892eab6ffac1e5d22244e0210c396fdfb207461f
Piyush <piyushjha1625@gmail.com>
2026-01-10 10:40:51 +0530

    Readme Files Changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import BottomNavigation from './components/BottomNavigation';
+import BottomNavigation from './components/BottomNavigation.jsx';
 import './App.css';
 
 function App() {
